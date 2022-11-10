8cc662a0a3f9543f93d592f12e381bf7e7fd4430
Blank67 <hmnshshrm67@gmail.com>
2022-11-10 20:32:53 +0530

    Expense-Tracker using Async await in Axios

--- ExpenseTracker/index.js
+++ ExpenseTracker/index.js
@@ -1,71 +1,97 @@
-"use strict"
+"use strict";
 
-const form = document.querySelector('#addForm');
-const mainList = document.querySelector('#mainList');
+const form = document.querySelector("#addForm");
+const mainList = document.querySelector("#mainList");
+const apiLink = `https://crudcrud.com/api/4b44d738373a46a0b6e298ef1a0f6bce/expense-tracker`;
 
-form.addEventListener('submit',saveToLocal);
-document.addEventListener('DOMContentLoaded',refresh);
+form.addEventListener("submit", save);
+document.addEventListener("DOMContentLoaded", refresh);
 
-function refresh(){
-    axios
-        .get('https://crudcrud.com/api/57f3ae02f0eb4e6d9abfc96a5aac5121/expense-tracker')
-        .then((res) => {
-            res.data.forEach(obj => {
-                console.log(obj);
-                showOnScreen(obj);
-            })
-        })
-        .catch((err) => {'Error Block: ',err})
+async function refresh() {
+    try {
+        // debugger;
+        const getResponse = await axios.get(apiLink);
+        // console.log(getResponse.data);
+        getResponse.data.forEach((obj) => {
+            // console.log(obj);
+            showOnScreen(obj);
+        });
+    } catch (error) {
+        console.log("Error in refresh().");
+    }
 }
 
-function saveToLocal(e){
-    e.preventDefault();
-    const amount = document.querySelector('#amount').value;
-    const description = document.querySelector('#description').value;
-    const category = document.querySelector('#category').value;
-    const expense = {
-        amount,
-        description,
-        category
-    };
-    axios
-        .post('https://crudcrud.com/api/57f3ae02f0eb4e6d9abfc96a5aac5121/expense-tracker',expense)
-        .then(() => {
-            showOnScreen(expense);
-        })
-        .catch((err) => {console.log("Error Block: ",err);})
+async function save(e) {
+    try {
+        // debugger;
+        e.preventDefault();
+        const amount = document.querySelector("#amount").value;
+        const description = document.querySelector("#description").value;
+        const category = document.querySelector("#category").value;
+        const expense = {
+            amount,
+            description,
+            category,
+        };
+        try {
+            const postResponse = await axios.post(apiLink, expense);
+            // console.log(postResponse.data);
+            showOnScreen(postResponse.data);
+        } catch (error) {
+            console.log("Error in post request in save().");
+        }
+    } catch (error) {
+        console.log("Error at start of save()");
+    }
 }
 
-function showOnScreen(expObj){
-    form.reset();
-    removeExpenseFromScreen(expObj.description);
-    const liHTML = `<li class="list-group-item" id="${expObj._id}"> Amount- Rs. ${expObj.amount}<br>Description- ${expObj.description}<br>Category- ${expObj.category}
+function showOnScreen(expObj) {
+    try {
+        // debugger;
+        form.reset();
+        removeExpenseFromScreen(expObj._id);
+        const liHTML = `<li class="list-group-item" id="${expObj._id}"> Amount- Rs. ${expObj.amount}<br>Description- ${expObj.description}<br>Category- ${expObj.category}
                     <span id=${expObj.description} hidden>${expObj}</span>
                     <button class="btn btn-danger btn-sm float-right delete" onclick=deleteExpense('${expObj._id}') style="margin-left:5px">Delete</button>
                     <button class="btn btn-danger btn-sm float-right delete" style="background-color:#f4f4f4; color:black" onclick=editExpense('${expObj.description}','${expObj.amount}','${expObj.category}','${expObj._id}')>Edit</button>                    
                     </li>`;
-    mainList.innerHTML = mainList.innerHTML+liHTML; 
+        mainList.innerHTML = mainList.innerHTML + liHTML;
+    } catch (error) {
+        console.log("Error in showOnScreen().");
+    }
 }
 
-function deleteExpense(uniqID){  
-        axios
-            .delete(`https://crudcrud.com/api/57f3ae02f0eb4e6d9abfc96a5aac5121/expense-tracker/${uniqID}`)
-            .then(() => {
-                removeExpenseFromScreen(uniqID);
-            })
-            .catch((err) => {console.log("Error Block: ",err);})
+async function deleteExpense(uniqID) {
+    try {
+        // debugger;
+        const deleteResponse = await axios.delete(`${apiLink}/${uniqID}`);
+        // console.log(deleteResponse.status);
+        removeExpenseFromScreen(uniqID);
+    } catch (error) {
+        console.log("Error in deleteExpense().");
+    }
 }
 
-function editExpense(description,amount,category,uniqID){
-    document.querySelector('#amount').value = amount;
-    document.querySelector('#description').value = description;
-    document.querySelector('#category').value = category;
-    deleteExpense(uniqID);
+function editExpense(description, amount, category, uniqID) {
+    // debugger;
+    try {
+        document.querySelector("#amount").value = amount;
+        document.querySelector("#description").value = description;
+        document.querySelector("#category").value = category;
+        deleteExpense(uniqID);
+    } catch (error) {
+        console.log("Error in editExpense().");
+    }
 }
 
-function removeExpenseFromScreen(uniqID){
-    const expenseToBeDeleted = document.getElementById(`${uniqID}`);
-    if(expenseToBeDeleted != null){
-        mainList.removeChild(expenseToBeDeleted);
+function removeExpenseFromScreen(uniqID) {
+    try {
+        // debugger;
+        const expenseToBeDeleted = document.getElementById(`${uniqID}`);
+        if (expenseToBeDeleted != null) {
+            mainList.removeChild(expenseToBeDeleted);
+        }
+    } catch (error) {
+        console.log("Error in removeExpenseFromScreen().");
     }
 }
